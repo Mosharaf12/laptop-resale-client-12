bad9c919ca90a6e2fc39e5dd7a26133e74bb874d
unknown <mosharafctg111@gmail.com>
2022-11-24 12:14:48 +0600

    added the favicon

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+
 import Banner from './Banner/Banner';
+import LaptopSection from './LaptopSection/LaptopSection';
 
 const Home = () => {
+    
     return (
         <div>
+            
             <Banner></Banner>
+            <LaptopSection></LaptopSection>
         </div>
     );
 };
