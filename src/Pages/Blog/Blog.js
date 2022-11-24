4d6a006d3a1e91eb58da314b4d65f96b8eabe4a4
unknown <mosharafctg111@gmail.com>
2022-11-24 13:43:02 +0600

    blog section completed

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Blog = () => {
     return (
-        <section className="bg-gray-100 text-gray-800">
+        <section className="text-gray-800 my-12">
         <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
-            <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">Frequently Asked Questions</h2>
+            <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">Question About Programming </h2>
             <div className="divide-y divide-gray-300">
                 <div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
                     <h3 className="font-semibold md:col-span-5">1 What are the different ways to manage a state in a React application?</h3>
@@ -25,21 +25,26 @@ const Blog = () => {
                 <div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
                     <h3 className="font-semibold md:col-span-5">3 What is a unit test? Why should we write unit tests?</h3>
                     <p className="md:pl-0 md:col-span-7">A unit test is a way of testing a unit - the smallest piece of code that can be logically isolated in a system. In most programming languages, that is a function, a subroutine, a method or property. The isolated part of the definition is important.
-                    </p>
-                    <p>They enable you to catch bugs early in the development process. Automated unit tests help a great deal with regression testing. They detect code smells in your codebase. For example, if you're having a hard time writing unit tests for a piece of code, it might be a sign that your function is too complex.</p>
+                    <br />
+                    <br />
+                    They enable you to catch bugs early in the development process. Automated unit tests help a great deal with regression testing. They detect code smells in your codebase. For example, if you're having a hard time writing unit tests for a piece of code, it might be a sign that your function is too complex.</p>
                 </div>
                 <div className="py-6 space-y-2 md:grid md:grid-cols-12 md:gap-8 md:space-y-0">
                     <h3 className="font-semibold md:col-span-5">4 React vs. Angular vs. Vue?</h3>
-                    <p className="md:pl-0 md:col-span-7"> <strong>React: </strong> It's used for building interactive user interfaces and web applications quickly and efficiently with significantly less code than you would with vanilla JavaScript. In React, you develop your applications by creating reusable components that you can think of as independent Lego blocks.
+                    <p className="md:pl-0 md:col-span-7">React: It's used for building interactive user interfaces and web applications quickly and efficiently with significantly less code than you would with vanilla JavaScript. In React, you develop your applications by creating reusable components that you can think of as independent Lego blocks.
                     <br />
                     <br />
-                    <strong>Angular:</strong> This topic can help you understand Angular: what Angular is, what advantages it provides, and what you might expect as you start to build your applications.
+
+                    Angular: This topic can help you understand Angular: what Angular is, what advantages it provides, and what you might expect as you start to build your applications.
                     A component-based framework for building scalable web applications.
-                    </p>
                     <br />
                     <br />
-                    <p> <strong>Vue:</strong> Vue is a JavaScript framework for building user interfaces. It builds on top of standard HTML, CSS, and JavaScript and provides a declarative and component-based programming model that helps you efficiently develop user interfaces, be they simple or complex.</p>
+
+                    Vue: Vue is a JavaScript framework for building user interfaces. It builds on top of standard HTML, CSS, and JavaScript and provides a declarative and component-based programming model that helps you efficiently develop user interfaces, be they simple or complex.
+</p>
                 </div>
+             
+                
             </div>
         </div>
     </section>
